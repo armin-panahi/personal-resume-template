ad191d5fda080ec7d2437532319f9a2039c74474
ARMIN PANAHI <amirhosseinpanahi.business@gmail.com>
2026-08-27 02:37:22 +0430

    Change logo type

--- src/sections/render.js
+++ src/sections/render.js
@@ -10,7 +10,7 @@ import {
 
 export function renderNavigation() {
   const logo = document.getElementById('nav-logo');
-  logo.innerHTML = `${icons.logo}<span>نام شما.dev</span>`;
+  logo.innerHTML = `${icons.logo}<span>لوگو شما</span>`;
 
   const navLinks = document.getElementById('nav-links');
   const mobileNavLinks = document.getElementById('mobile-nav-links');
